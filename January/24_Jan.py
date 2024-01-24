e31e071049b50fa84d19b3893bfc1047d94d6713
Pallavi Pandey <96332940+Pallavi-Pandey@users.noreply.github.com>
2024-01-24 22:01:19 +0530

    24_Jan.py

---
+++ January/24_Jan.py
@@ -0,0 +1,75 @@
+# Valid parentheses
+# Google Interview Qs
+# https://leetcode.com/problems/valid-parentheses/
+
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+ 
+
+# Example 1:
+
+# Input: s = "()"
+# Output: true
+# Example 2:
+
+# Input: s = "()[]{}"
+# Output: true
+
+# Solution:
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
+
+
+# Group Anagrams
+# Samsung + Adobe + Amazon Interview Qs
+# https://leetcode.com/problems/group-anagrams/
+
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+# Example 1:
+
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+# Example 2:
+
+# Input: strs = [""]
+# Output: [[""]]
+# Example 3:
+
+# Input: strs = ["a"]
+# Output: [["a"]]
+
+# Solution:
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs):
+        anagrams = defaultdict(list)
+
+        for word in strs:
+            # Sort the characters in the word and use it as a key for grouping
+            sorted_word = ''.join(sorted(word))
+            anagrams[sorted_word].append(word)
+
+        return list(anagrams.values())
